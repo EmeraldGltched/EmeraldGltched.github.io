c6a9934128ea8f4d539f4628ab6cd592b4f8ea37
Gltched <78609482+Gltched@users.noreply.github.com>
2021-03-25 13:28:43 -0700

    Feedback page test

---
+++ SpaceExplorer - Client/Feedback/feedback.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Feedback</title>
+    <link rel="stylesheet" href="./style.css">
+  </head>
+  <body>
+    <h1 class="page-title">What do you think of SpaceExplorer?</h1>
+  </body>
+</html>
